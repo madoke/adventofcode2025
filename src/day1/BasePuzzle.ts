5f843aed81c007dc779c266df43b94568346c41c
David Simao <madoke@gmail.com>
2025-12-02 01:03:53 +0000

    add commander

--- src/day1/BasePuzzle.ts
+++ src/day1/BasePuzzle.ts
@@ -1,12 +1,10 @@
 import { Direction, type DialState, type Instruction } from "./types.js";
 
-const INITIAL_DIAL_STATE = { position: 50 };
-
 export abstract class BasePuzzle {
-	constructor(protected dialState: DialState = INITIAL_DIAL_STATE) {}
+	constructor(protected dialState: DialState = { position: 50 }) {}
 
 	protected resetDial(): void {
-		this.dialState = INITIAL_DIAL_STATE;
+		this.dialState = { position: 50 };
 	}
 
 	protected rotateDial(instruction: Instruction): {
@@ -18,19 +16,22 @@ export abstract class BasePuzzle {
 			instruction.direction === Direction.Left
 				? this.dialState.position - instruction.distance
 				: this.dialState.position + instruction.distance;
+		const initialPosition = this.dialState.position;
 
-		if (nextPosition < 0 || nextPosition > 99) {
-			while (nextPosition > 99) {
-				nextPosition -= 100;
-				timesPastZero += 1;
-			}
-
-			while (nextPosition < 0) {
-				nextPosition += 100;
-				timesPastZero += 1;
-			}
-		} else if (nextPosition === 0) {
+		if (nextPosition === 0) {
 			timesPastZero += 1;
+		} else if (nextPosition > 99) {
+			const quotient = Math.floor(nextPosition / 100);
+			timesPastZero += quotient;
+			nextPosition = nextPosition % 100;
+		} else if (nextPosition < 0) {
+			const quotient = Math.floor(nextPosition / -100);
+			if (initialPosition === 0) {
+				timesPastZero += quotient;
+			} else {
+				timesPastZero += 1 + quotient;
+			}
+			nextPosition = 100 + (nextPosition % 100);
 		}
 
 		this.dialState.position = nextPosition;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
+import { program } from "commander";
 import { Puzzle1 } from "./day1/puzzle1/index.js";
 import { Puzzle2 } from "./day1/puzzle2/index.js";
 
-new Puzzle1().run("input.txt").then(() => {
-	console.log("Done");
-});
-new Puzzle2().run("input.test.txt").then(() => {
-	console.log("Done");
-});
-new Puzzle2().run("input.test1.txt").then(() => {
-	console.log("Done");
-});
-new Puzzle2().run("input.txt").then(() => {
-	console.log("Done");
-});
+program
+	.requiredOption("--day <number>", "Specify the day to run")
+	.requiredOption("--puzzle <number>", "Specify the puzzle to run")
+	.argument("<string>");
+
+program.parse();
+
+const day = program.opts().day;
+const puzzleNumber = program.opts().puzzle;
+const inputFileName = program.args[0];
+
+if (!day) {
+	console.error("Day is required");
+	process.exit(1);
+}
+
+if (!puzzleNumber) {
+	console.error("Puzzle number is required");
+	process.exit(1);
+}
+
+if (!inputFileName) {
+	console.error("Input file name is required");
+	process.exit(1);
+}
+
+const puzzles = [[new Puzzle1(), new Puzzle2()]];
+const puzzle = puzzles[day - 1]?.[puzzleNumber - 1];
+
+if (!puzzle) {
+	console.error("Invalid day or puzzle number");
+	process.exit(1);
+}
+
+await puzzle.run(inputFileName);
